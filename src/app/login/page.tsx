7874ac4bd7a18ed290713224cda597eaf4c3bfa0
Shahidul-Islam-Jony <shahidulislam13sf@gmail.com>
2024-01-22 22:34:31 +0600

    Registration page created

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { FcGoogle } from "react-icons/fc";
-import { FaFacebook,FaEye ,FaEyeSlash} from "react-icons/fa";
+import { FaFacebook, FaEye, FaEyeSlash } from "react-icons/fa";
 import loginImg from '../../assests/Login-Registration/login4.jpg'
 import Link from "next/link";
 import { useState } from "react";
@@ -8,15 +8,22 @@ import { useState } from "react";
 
 const Login = () => {
     const [showPass, setShowPass] = useState(false);
+    const handleLogin = e => {
+        e.preventDefault();
+        const form = e.target;
+        const email = form.email.value;
+        const pass = form.password.value;
+        console.log(pass,email);
+    }
 
     return (
-        <div className="min-h-screen bg-cover bg-opacity-80" style={{ backgroundImage: `url(${loginImg.src})` }}>
+        <div className="min-h-screen bg-cover bg-opacity-30" style={{ backgroundImage: `url(${loginImg.src})` }}>
             <div className="w-11/12 mx-auto">
                 <div className="w-fit mx-auto">
                     <div className='pt-16'>
                         <h1 className="text-5xl font-bold mb-4 text-white text-center">Login now!</h1>
                         <div className="rounded-lg shadow-2xl backdrop-blur-sm border-2 border-indigo-600 bg-transparent text-white px-4 py-6">
-                            <form >
+                            <form onSubmit={handleLogin}>
                                 <div>
                                     <label className="label">
                                         <span className="text-xl font-medium">Email</span>
@@ -27,8 +34,8 @@ const Login = () => {
                                     <label className="label">
                                         <span className="text-xl font-medium">Password</span>
                                     </label>
-                                    <input type={showPass ? 'text':'password'} name='password' placeholder="password" className="h-6 focus:h-8 relative rounded-md px-2 outline-none w-full border-b-2 bg-transparent" required />
-                                    <span onClick={()=>setShowPass(!showPass)} className="absolute right-7 text-xl">
+                                    <input type={showPass ? 'text' : 'password'} name='password' placeholder="password" className="h-6 focus:h-8 relative rounded-md px-2 outline-none w-full border-b-2 bg-transparent" required />
+                                    <span onClick={() => setShowPass(!showPass)} className="absolute right-7 text-xl">
                                         {
                                             showPass ? <FaEye /> : <FaEyeSlash />
                                         }
@@ -42,10 +49,10 @@ const Login = () => {
                                 </div>
                             </form>
                             <div>
-                                <div className="divider divider-primary text-indigo-600 font-bold">OR</div>
+                                <div className="divider divider-primary text-indigo-600 font-bold">OR Login With</div>
                                 <div className="flex gap-2">
-                                    <button className='btn btn-outline text-white w-1/2 text-lg hover:bg-primary border-blue-600 capitalize'><FcGoogle className='text-3xl mr-4'></FcGoogle>Login With Google</button>
-                                    <button className='btn btn-outline text-white w-1/2 text-lg hover:bg-primary border-blue-600 capitalize'><FaFacebook className='text-3xl'></FaFacebook>Login With Facebook</button>
+                                    <button className='btn btn-outline text-white w-1/2 text-lg hover:bg-primary border-blue-600 capitalize'><FcGoogle className='text-3xl mr-4'></FcGoogle>Google</button>
+                                    <button className='btn btn-outline text-white w-1/2 text-lg hover:bg-primary border-blue-600 capitalize'><FaFacebook className='text-3xl'></FaFacebook>Facebook</button>
                                 </div>
                             </div>
                         </div>
