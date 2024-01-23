44ae0f58678650fba83946a553e8d06fae418430
shahidul-islam-jony <shahidulislam13sf@gmail.com>
2024-01-23 18:13:24 +0600

    Firebase api key stored dotenv file and implemented google and email pass auth

--- src/app/(withLayout)/layout.tsx
+++ src/app/(withLayout)/layout.tsx
@@ -1,13 +1,17 @@
 import Footer from "@/Components/Shared/Footer/Footer";
 import Navbar from "@/Components/Shared/Navbar/Navbar";
+import AuthProvider from "@/providers/AuthProvider";
+
 
 const Layout = ({ children }) => {
     return (
-        <div>
-            <Navbar/>
-            {children}
-            <Footer/>
-        </div>
+        <AuthProvider>
+            <div>
+                <Navbar />
+                {children}
+                <Footer />
+            </div>
+        </AuthProvider>
     );
 };
 
