ff958f44492dcbcced3f7ee124159ca76fdf79aa
Morsed <mdmorsedalam324@gmail.com>
2024-01-25 15:28:03 +0600

    successfully get data using tanstack query and mongoose

---
+++ src/Hooks/usePricings.tsx
@@ -0,0 +1,32 @@
+// "use client";
+import useAxiosPublic from "./useAxiosPublic";
+import { useQuery } from "@tanstack/react-query";
+
+interface pricingsData {
+  id: string;
+  benifit1: string;
+  benifit2: string;
+  benifit3: string;
+  benifit4: string;
+  price: number;
+  recommended: string;
+  btnColor: string;
+  badg: string;
+}
+const usePricings = () => {
+  const axiosPublic = useAxiosPublic();
+  const {
+    data: pricings = [],
+    isLoading: loading,
+    refetch,
+  } = useQuery<pricingsData[]>({
+    queryKey: ["pricings"],
+    queryFn: async () => {
+      const res = await axiosPublic("/pricing");
+      return res.data;
+    },
+  });
+  return [pricings, loading, refetch];
+};
+
+export default usePricings;
