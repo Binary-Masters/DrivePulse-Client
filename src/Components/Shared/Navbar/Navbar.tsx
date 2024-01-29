95b8925aabc92336fc8ce19c3829df8fd1039758
= <=>
2024-01-29 17:12:33 +0600

    [fix](Private Route): Reloading dashboard while user is logged in no longer redirects to login page

--- src/Components/Shared/Navbar/Navbar.tsx
+++ src/Components/Shared/Navbar/Navbar.tsx
@@ -18,8 +18,6 @@ export default function Navbar() {
     { path: "/", label: "Home" },
     { path: "/about", label: "About" },
     { path: "/pricing", label: "Pricing" },
-    { path: "/developers", label: "Developers" },
-    { path: "/dashboard", label: "Dashboard" },
     { path: "/contact", label: "Contact" },
     { path: "/register", label: "Register" },
     { path: "/login", label: "Login" },
