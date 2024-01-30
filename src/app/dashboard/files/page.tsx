c20fdb45a956d3678e42231c0109dc155b690979
Kj-RahiL <rahiilarham@gmail.com>
2024-01-30 20:36:53 +0600

    add share and rename poup

--- src/app/dashboard/files/page.tsx
+++ src/app/dashboard/files/page.tsx
@@ -6,7 +6,7 @@ import { MdArrowDropDownCircle, MdDelete } from "react-icons/md";
 
 const Files = () => {
     return (
-        <div>
+        <div className="mt-20">
             <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
                 <table className="w-full  text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                     <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
