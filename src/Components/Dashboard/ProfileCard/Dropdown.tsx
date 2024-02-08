19dc9aaaba5bd8fe026f495b37afc77028f47834
Morsed <mdmorsedalam324@gmail.com>
2024-02-08 18:10:04 +0600

    Some changes

--- src/Components/Dashboard/ProfileCard/Dropdown.tsx
+++ src/Components/Dashboard/ProfileCard/Dropdown.tsx
@@ -1,21 +1,25 @@
-import {  Settings } from "lucide-react";
+import { Settings } from "lucide-react";
 import Link from "next/link";
 const Dropdown = () => {
   return (
-    <div className='absolute top-0 right-3 p-4'>
+    <div className="absolute top-0 right-3 p-4">
       <div className="dropdown dropdown-bottom">
         <div tabIndex={0} role="button" className=" m-1">
-         <Settings/>
+          <Settings />
         </div>
         <ul
           tabIndex={0}
-          className="dropdown-content z-[1] menu  shadow   absolute right-1  rounded-xl   "
+          className="dropdown-content z-[1] menu  shadow absolute -right-8  rounded-xl   "
         >
-          <button className="btn btn-accent btn-sm my-3">Edit</button>
+          <button className="btn btn-accent btn-md mb-1 text-sm">
+            Edit Profile
+          </button>
           {/*dynamic path  */}
-         {/* /dashboard/profile/password */}
-          <Link href='/dashboard/profile/password'>
-          <button className="btn btn-accent btn-sm">Password</button>
+          {/* /dashboard/profile/password */}
+          <Link href="/dashboard/profile/password">
+            <button className="btn btn-accent btn-md text-sm">
+              Change Password
+            </button>
           </Link>
         </ul>
       </div>
--- src/app/dashboard/profile/page.tsx
+++ src/app/dashboard/profile/page.tsx
@@ -1,12 +1,11 @@
-import React from 'react'
-import ProfileCard from '@/Components/Dashboard/ProfileCard/ProfileCard'
-import Dropdown from '@/Components/Dashboard/ProfileCard/Dropdown'
+import React from "react";
+import ProfileCard from "@/Components/Dashboard/ProfileCard/ProfileCard";
 const Profile = () => {
   return (
-    <div className='min-h-screen  classes flex justify-center items-center '> 
-      <ProfileCard/>  
+    <div className="min-h-screen  classes flex justify-center items-center ">
+      <ProfileCard />
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
