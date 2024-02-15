6af2739315d269f292a085cf47db65b827ed341c
= <=>
2024-02-15 20:20:20 +0600

    [feat](File Management System): File and folder(empty) deletion functional

---
+++ src/Utils/Files/handleDeleteNode/fragments/deleteFolderFunc.ts
@@ -0,0 +1,45 @@
+import getFolderPathData from "@/Utils/FolderNavigation/getFolderPathData";
+import Swal from "sweetalert2";
+
+const deleteFolderFunc = async (hookPropObj: any, fullPath: string) => {
+	const { axiosInstance, user, setPath } = hookPropObj;
+	const { currentPath, parentPath } = getFolderPathData(fullPath, "folder", user);
+
+	const { data } = await axiosInstance.get(
+		`/files?rootdir=${user?.uid}&path=${currentPath}`
+	);
+
+	if (data.length) {
+		Swal.fire({
+			title: "Folder not empty",
+			text: "Cannot delete non-empty folders",
+			icon: "error",
+		});
+	} else {
+		axiosInstance
+			.delete(`/files?fullPath=${fullPath}`)
+			.then((res) => {
+				if (res.data.deletedCount > 0) {
+					Swal.fire({
+						title: `Folder deleted successfully`,
+						icon: "success",
+					}).then(() => {
+						setPath(parentPath);
+					});
+				} else {
+					Swal.fire({
+						title: "Something went Wrong",
+						icon: "error",
+					});
+				}
+			})
+			.catch((error) => {
+				Swal.fire({
+					title: error.message,
+					icon: "error",
+				});
+			});
+	}
+};
+
+export default deleteFolderFunc;
