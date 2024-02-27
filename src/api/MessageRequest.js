522d01699c911afce45796d0df8830160adcb66f
Shahidul-Islam-Jony <shahidulislam13sf@gmail.com>
2024-02-27 21:07:09 +0600

    merge message request conflicts

--- src/api/MessageRequest.js
+++ src/api/MessageRequest.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-
-const API = axios.create({ baseURL: 'https://drive-pulse-server.vercel.app' });
 const API = axios.create({ baseURL: "http://localhost:3001" });
 
 export const getMessages = (id) => API.get(`/message/${id}`);
